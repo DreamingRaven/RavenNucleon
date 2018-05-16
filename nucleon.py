2f68fe252a21faf12511adb98da15ab8df9d5af1
DreamingRaven <geo.george@live.co.uk>
2018-05-16 14:27:34 +0000

    some minor edits and tests of json

--- nucleon.py
+++ nucleon.py
@@ -1,4 +1,17 @@
-import os, sys
+# @Author: George Onoufriou <GeorgeRaven>
+# @Date:   2018-05-16
+# @Project: RavenNucleon
+# @Filename: nucleon.py
+# @Last modified by:   GeorgeRaven
+# @Last modified time: 2018-05-16
+# @License: Please see LICENSE file in project root
+
+
+
+import os, sys, json
 from src import helpers
 
+# this file serves as the RavenNucleon entry point
+
 args = helpers.argz(sys.argv[1:])
+print(json.loads(json.dumps(args)))
--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,13 @@
+# @Author: George Onoufriou <GeorgeRaven>
+# @Date:   2018-05-16
+# @Project: RavenNucleon
+# @Filename: helpers.py
+# @Last modified by:   GeorgeRaven
+# @Last modified time: 2018-05-16
+# @License: Please see LICENSE file in project root
+
+
+
 import argparse
 import os, sys
 home = os.path.expanduser("~")
@@ -7,13 +17,15 @@ prePend = "[ " + name + " ] "
 def argz(argv=None, description=None):
 
     if(description == None):
-        description = "Nucleon"
+        description = "RavenNucleon"
 
     parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument("-d", "--device",     default="",
-        help="device name to install archlinux on")
+    parser.add_argument("-d", "--device",       default="", required=True,
+        help="device name to install archlinux on e.g: \'/dev/sdd\'")
+    parser.add_argument("-v", "--verbose",      default=0, type=int,
+        help="sets verbosity level; how much information is displayed")
     parser.add_argument("--noconfirm", default=False, action="store_true",
-        help="for those that live life on the... ledge.")
+        help="for those that live life on the... ledge")
 
     return vars(parser.parse_args(argv))
