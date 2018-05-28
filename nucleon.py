593f8389356c4bff7cb8fc0da7fe52a8e4ac7113
DreamingRaven <geo.george@live.co.uk>
2018-05-28 17:38:49 +0000

    changed nucleon formatting

--- nucleon.py
+++ nucleon.py
@@ -18,21 +18,22 @@ def main():
 # declaring usefull global variables
 home = os.path.expanduser("~")
 name = os.path.basename(sys.argv[0])
+
 fileAndPath = inspect.getframeinfo(inspect.currentframe()).filename
 path = os.path.dirname(os.path.abspath(fileAndPath))
+
 prePend = "[ " + name + " ] "
 description = name + "; " + "Python script entry point for RavenNucleon\
  so that archlinux may reign supreme on whatever you choose for Nucleon\
 to install it on."
 
-# capture arguments in dict
+# capture arguments in dict then put into json for bash
 args = argz(sys.argv[1:], description=description)
-# create json version of dict to pass to bash
 args_json = json.loads(json.dumps(args))
 
 # setting up fallback logger
 log = Log(logLevel=args["loglevel"])
-print = log.print # overiding default print with fallback logger
+print = log.print
 
 # installing, updating, upgrading
 installer(path=path)
